159bc3fdf1a13385a287fe0bbedde658717a6762
Nandhini Venkatesan <x20236158@student.ncirl.ie>
2022-04-18 03:52:57 +0100

    Add project files.

---
+++ FarmSystem.Test1/EmydexFarmSystem.cs
@@ -0,0 +1,109 @@
+﻿using FarmSystem.Test2;
+using System;
+using System.Collections.Generic;
+
+namespace FarmSystem.Test1
+{
+    public class EmydexFarmSystem
+    {
+       public static List<string> list = new List<string>();
+        public static List<string> releasedAnimals = new List<string>();
+        //TEST 1
+        public List<string> Enter(string animal)
+        {
+            //TODO Modify the code so that we can display the type of animal (cow, sheep etc) 
+            //Hold all the animals so it is available for future activities
+            list.Add(animal + " has entered the Emydex");
+            return list;
+            
+        }
+     
+        //TEST 2
+        public void MakeNoise(List<string> animals)
+        {
+            foreach(string s in animals)
+            {
+                if (s.Contains("Cow"))
+                {
+                    Cow cow = new Cow();
+                    cow.Talk();
+                }
+                if (s.Contains("Hen"))
+                {
+                    Hen hen = new Hen();
+                    hen.Talk();
+                }
+                if (s.Contains("Horse"))
+                {
+                    Horse horse = new Horse();
+                    horse.Talk();
+                }
+                if (s.Contains("Sheep"))
+                {
+                    Sheep sheep = new Sheep();
+                    sheep.Talk();
+                }
+                
+            }
+            //Test 2 : Modify this method to make the animals talk
+           // 
+        }
+
+        //TEST 3
+        public void MilkAnimals(List<string> animals)
+        {
+            if (animals.Count == 0)
+            {
+                    Console.WriteLine("Cannot identify the farm animals which can be milked");
+                
+            }
+            else
+            {
+                foreach (string s in animals)
+                {
+                    if (s.Contains("Cow"))
+                    {
+                        Console.WriteLine("Cow was milked!");
+                    }
+
+                }
+            }
+        }
+
+        //TEST 4
+        public List<string> ReleaseAllAnimals(List<string> animals)
+        {
+            if(animals.Count!=0)
+            {
+                foreach(string s in animals)
+                {
+                    if (s.Contains("Cow"))
+                    {
+                        releasedAnimals.Add("Cow has left the farm");
+
+                    }
+                    if (s.Contains("Hen"))
+                    {
+                        releasedAnimals.Add("Hen has left the farm");
+                    }
+                    if (s.Contains("Horse"))
+                    {
+                        releasedAnimals.Add("Horse has left the farm");
+                    }
+                    if (s.Contains("Sheep"))
+                    {
+                        releasedAnimals.Add("Sheep has left the farm");
+                    }
+                }
+                list.Clear();
+                if (list.Count == 0)
+                    releasedAnimals.Add("Emydex Farm is now empty");
+                else
+                    releasedAnimals.Add("There are still animals in the farm, farm is not free");
+
+            }
+            return releasedAnimals;
+          // Console.WriteLine("There are still animals in the farm, farm is not free");
+        }
+    }
+}
---
+++ FarmSystem.Test1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmSystem.Test1
+{
+    internal class Program
+    {
+       
+        private static void Main(string[] args)
+        {
+            Excercise1();
+            Excercise2();
+            Excercise3();
+            Excercise4();
+            Console.ReadKey();
+        }
+
+        /************************************************************************************************************
+        Exercise 1 : Apply OOP concepts (abstraction and encapsulation) to the classes 
+        modify the code to get the output below
+        Cow has entered the farm
+        Hen has entered the farm
+        Horse has entered the farm
+        Sheep has entered the farm
+        ***************************************************************************************************************/
+        private static List<string> listOfAnimals = new List<string>();
+       
+
+        private static void Excercise1()
+        {
+            Console.WriteLine("Exercise 1 : Press any key when it is time to open the Farm to animals");
+            Console.ReadKey();
+            common();
+            Console.ReadKey();
+        }
+
+        private static void common()
+        {
+            var farm = new EmydexFarmSystem();
+            Cow cow = new Cow();
+            cow.Id = "Cow";
+            cow.NoOfLegs = 4;
+            listOfAnimals = farm.Enter(cow.Id);
+
+            Hen hen = new Hen();
+            hen.Id = "Hen";
+            hen.NoOfLegs = 2;
+            listOfAnimals=farm.Enter(hen.Id);
+
+            Horse horse = new Horse();
+            horse.Id = "Horse";
+            horse.NoOfLegs = 4;
+            listOfAnimals=farm.Enter(horse.Id);
+
+            Sheep sheep = new Sheep();
+            sheep.Id = "Sheep";
+            sheep.NoOfLegs = 4;
+            listOfAnimals=farm.Enter(sheep.Id);
+
+            foreach(string s in listOfAnimals)
+            {
+                Console.WriteLine(s);
+            }
+
+            
+        }
+
+/***************************************************************************************************************
+ Test Excercise 2
+ If you have completed the first test excercise, you can continue with the second one
+ Modify the program and EmydexFarmSystem.MakeNoise() method to get the below output
+ Expected Test 2 Program Output
+
+ Exercise 2 : Press any key to scare the animals in the farm
+    Cow has entered the farm
+    Hen has entered the farm
+    Horse has entered the farm
+    Sheep has entered the farm
+    Cow says Moo!
+    Hen says CLUCKAAAAAWWWWK!
+    Horse says Neigh!
+    Sheep says baa!
+ *****************************************************************************************************************/
+        private static void Excercise2()
+        {
+            //TODO : Apply OOP concepts and modify the code below to get the required output 
+            Console.WriteLine("Exercise 2 : Press any key to scare the animals in the farm");
+            Console.ReadKey();
+            foreach (string s in listOfAnimals)
+            {
+                Console.WriteLine(s);
+            }
+           
+            var farm = new EmydexFarmSystem();
+            farm.MakeNoise(listOfAnimals);
+
+            Console.ReadKey();
+        }
+
+/*****************************************************************************************************************
+Test Excercise 3
+If you have completed the previous test excercise, you can continue with this one 
+The project includes an interface IMilkableAnimal. Make use of this interface to implement on the relevant classes
+so that calling the EmydexFarmSystem.MilkAnimals() method to get the below output
+
+Expected Test 3 Program Output
+
+Exercise 3 : Press any key when it is time to milk animals
+Cow has entered the farm
+Hen has entered the farm
+Horse has entered the farm
+Sheep has entered the farm
+Cow was milked!
+************************************************************************************************************/
+        private static void Excercise3()
+        {
+            //TODO : Apply OOP concepts and modify the code below to get the required output 
+            Console.WriteLine("Exercise 3 : Press any key when it is time to milk animals");
+            Console.ReadKey();
+            foreach (string s in listOfAnimals)
+            {
+                Console.WriteLine(s);
+            }
+            var farm = new EmydexFarmSystem();
+            
+            farm.MilkAnimals(listOfAnimals);
+            Console.ReadKey();
+        }
+
+/****************************************************************************************************
+Test Excercise 4
+Modify the EmydexFarmSystem.ReleaseAllAnimals() so that all animals are released (simply clear the collection )
+Expose an event on the FarmSystem FarmEmpty which is invoked once all the animals are released
+Subscribe to that event in the main to get the expected output
+
+Expected Test 4 Program Output
+
+Exercise 4: Press any key to free all animals
+Cow has entered the farm
+Hen has entered the farm
+Horse has entered the farm
+Sheep has entered the farm
+Cow has left the farm
+Hen has left the farm
+Horse has left the farm
+Sheep has left the farm
+Emydex Farm is now empty
+********************************************************************************************************************/
+        private static void Excercise4()
+        {
+            //TODO : Apply OOP concepts and modify the code below to get the required output 
+            Console.WriteLine("Exercise 4: Press any key to free all animals");
+            Console.ReadKey();
+            foreach (string s in listOfAnimals)
+            {
+                Console.WriteLine(s);
+            }
+            var farm = new EmydexFarmSystem();
+            var releasedAnimals = farm.ReleaseAllAnimals(listOfAnimals);
+            foreach (string s in releasedAnimals)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
